5968d3c0112c3347ae43643d6019469991706772
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-04-20 19:02:18 +0930

    Add files via upload

--- headers/push_swap.h
+++ headers/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: prossi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 12:11:13 by prossi            #+#    #+#             */
-/*   Updated: 2022/04/20 16:20:07 by prossi           ###   ########.fr       */
+/*   Updated: 2022/04/20 18:39:01 by prossi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,35 +17,39 @@
 # include <stdlib.h>
 # include <stdio.h>
 
-//push
+//pa and pb (push)
+
 void	push_stack_a(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 void	push_stack_b(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 
-//swap
+//sa, sb and sab (swap)
+
 void	swap_stack_a(long *stack_a, int *numbers_in_stack_a);
 void	swap_stack_b(long *stack_b, int *numbers_in_stack_b);
 void	swap_stack_a_and_stack_b(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 
-//rotate
+//ra, rb and rr (rotate)
+
 void	rotate_stack_a(long *stack_a, int *numbers_in_stack_a);
 void	rotate_stack_b(long *stack_b, int *numbers_in_stack_b);
 void	rotate_stack_a_and_stack_b(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 
-//reserve rotate
+//rra, rrb and rrr (reserve rotate)
+
 void	reserve_rotate_stack_a(long *stack_a, int *numbers_in_stack_a);
 void	reserve_rotate_stack_b(long *stack_b, int *numbers_in_stack_b);
 void	reserve_rotate_stack_a_and_stack_b(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 
-//libft and extensions
+//Libft
+
 int		ft_strlen(const char *str);
 int		ft_isdigit(int i);
 int		ft_atoi(const char *str);
 
 char	**ft_split(char const *str, char c);
 
-void	free_string(char **str);
+//Stack A and Stack B Logic
 
-//logic
 int		number_limits(long *stack_a, int numbers_in_stack_a);
 int		repeated_numbers(long *stack_a, int numbers_in_stack_a);
 int     is_only_numbers_in_the_stacks(char *numbers);
@@ -54,18 +58,29 @@ int		numbers_input(long *stack_a, int *numbers_in_stack_a, char **argv, char**nu
 int		standard_input(long *stack_a, int *numbers_in_stack_a, char **argv);
 int		seperating_input_numbers(long *stack_a, int *numbers_in_stack_a, char **argv);
 
+void	free_string(char **str);
 void	free_stacks(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 void    ft_reallocation(long *pointer, int startsize, int newsize);
 void	parameters(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 
-//sort 3 numbers
+//Sort 3 Numbers
+
 void	sort_3_numbers(long *stack_a, int *numbers_in_stack_a);
 
-//sort 5 numbers and all of the functions in that file
+//Sort 5 Numbers and all of the functions in that file
+
 void	sort_5_numbers(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 void	rotate_stack_a_radix(long *stack_a, int *numbers_in_stack_a);
 void	reserve_rotate_stack_a_radix(long *stack_a, int *numbers_in_stack_a);
 void	first_stack(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 void	second_stack(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
+
+//Sort beyond 3 and 5 Numbers and all of the functons in that file
+
+void	sort_beyond_3_and_5_numbers(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
+void	sort_a_section_of_the_stack(long *array, int *stack_size);
+void	swap_stack_a_first_time(long *copy, long *copy_swap_stack_a, long *stack_a, int *numbers_in_stack_a);
+void	swap_stack_a_second_time(long *copy, long *copy_swap_stack_a, long *stack_a, int *numbers_in_stack_a);
+void	swap_stack_a_third_time(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 
 #endif
