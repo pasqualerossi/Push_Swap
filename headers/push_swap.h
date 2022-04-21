6a73ed6c067ad881a6f992c5e115d3faf87523ee
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-04-21 11:18:16 +0930

    Add files via upload

--- headers/push_swap.h
+++ headers/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: prossi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 12:11:13 by prossi            #+#    #+#             */
-/*   Updated: 2022/04/20 18:39:01 by prossi           ###   ########.fr       */
+/*   Updated: 2022/04/21 10:54:41 by prossi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,30 @@
 # include <stdlib.h>
 # include <stdio.h>
 
-//pa and pb (push)
+//pa (push stack a) and pb (push stack b)
 
 void	push_stack_a(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 void	push_stack_b(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 
-//sa, sb and sab (swap)
+//sa (swap stack a), sb (swap stack b) and sab (swap stack a and stack b at the same time)
 
 void	swap_stack_a(long *stack_a, int *numbers_in_stack_a);
 void	swap_stack_b(long *stack_b, int *numbers_in_stack_b);
 void	swap_stack_a_and_stack_b(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 
-//ra, rb and rr (rotate)
+//ra (rotate stack a), rb (rotate stack b) and rr (rotate stack a and stack b at the same time)
 
 void	rotate_stack_a(long *stack_a, int *numbers_in_stack_a);
 void	rotate_stack_b(long *stack_b, int *numbers_in_stack_b);
 void	rotate_stack_a_and_stack_b(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 
-//rra, rrb and rrr (reserve rotate)
+//rra (reverse rotate stack a), rrb (reserve rotate stack b) and rrr (reserve rotate both stack a and stack b at the same time)
 
 void	reserve_rotate_stack_a(long *stack_a, int *numbers_in_stack_a);
 void	reserve_rotate_stack_b(long *stack_b, int *numbers_in_stack_b);
 void	reserve_rotate_stack_a_and_stack_b(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 
-//Libft
+//libft (all libft functions required for this project)
 
 int		ft_strlen(const char *str);
 int		ft_isdigit(int i);
@@ -48,7 +48,7 @@ int		ft_atoi(const char *str);
 
 char	**ft_split(char const *str, char c);
 
-//Stack A and Stack B Logic
+//stack a and stack b logic 
 
 int		number_limits(long *stack_a, int numbers_in_stack_a);
 int		repeated_numbers(long *stack_a, int numbers_in_stack_a);
@@ -63,11 +63,11 @@ void	free_stacks(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *num
 void    ft_reallocation(long *pointer, int startsize, int newsize);
 void	parameters(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 
-//Sort 3 Numbers
+//sort 3 numbers in stack a
 
 void	sort_3_numbers(long *stack_a, int *numbers_in_stack_a);
 
-//Sort 5 Numbers and all of the functions in that file
+//sort 5 numbers in both stack a and stack b and all of the functions in that file
 
 void	sort_5_numbers(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 void	rotate_stack_a_radix(long *stack_a, int *numbers_in_stack_a);
@@ -75,12 +75,21 @@ void	reserve_rotate_stack_a_radix(long *stack_a, int *numbers_in_stack_a);
 void	first_stack(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 void	second_stack(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 
-//Sort beyond 3 and 5 Numbers and all of the functons in that file
+//sort beyond 3 and 5 numbers in both stack a and stack b and all of the functons in that file
 
 void	sort_beyond_3_and_5_numbers(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
 void	sort_a_section_of_the_stack(long *array, int *stack_size);
 void	swap_stack_a_first_time(long *copy, long *copy_swap_stack_a, long *stack_a, int *numbers_in_stack_a);
 void	swap_stack_a_second_time(long *copy, long *copy_swap_stack_a, long *stack_a, int *numbers_in_stack_a);
 void	swap_stack_a_third_time(long *stack_a, long *stack_b, int *numbers_in_stack_a, int *numbers_in_stack_b);
+
+//int main functions required to run the push_swap program
+
+void	free_and_exit(int *numbers_in_stack_a, int *numbers_in_stack_b);
+void	free_and_exit_without_errors(int *numbers_in_stack_a, int *numbers_in_stack_b);
+void	check_parameters(int argc, char **argv, int *numbers_in_stack_a, int *numbers_in_stack_b);
+void	set_numbers(int *numbers_in_stack_a, int *numbers_in_stack_b, int argc);
+
+int		main(int argc, char **argv);
 
 #endif
