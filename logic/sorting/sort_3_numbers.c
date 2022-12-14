d081010acec62e7e044d2551818666b8ea8dc61a
Pasquale Emilio Rossi <58959408+pasqualerossi@users.noreply.github.com>
2022-12-14 13:23:24 +1030

    Update sort_3_numbers.c

--- logic/sorting/sort_3_numbers.c
+++ logic/sorting/sort_3_numbers.c
@@ -15,25 +15,19 @@
 void	sort_3_numbers(long *stack_a, int *numbers_in_stack_a)
 {
 	if ((stack_a[0] > stack_a[1]) && (stack_a[1] < stack_a[2]) && (stack_a[0] < stack_a[2]))
-	{
 		swap_stack_a(stack_a, numbers_in_stack_a);
-	}
 	if ((stack_a[0] > stack_a[1]) && (stack_a[1] > stack_a[2]) && (stack_a[0] > stack_a[2]))
 	{
 		swap_stack_a(stack_a, numbers_in_stack_a);
 		reverse_rotate_stack_a(stack_a, numbers_in_stack_a);
 	}
 	if ((stack_a[0] > stack_a[1]) && (stack_a[1] < stack_a[2]) && (stack_a[0] > stack_a[2]))
-	{
 		rotate_stack_a(stack_a, numbers_in_stack_a);
-	}
 	if ((stack_a[0] < stack_a[1]) && (stack_a[1] > stack_a[2]) && (stack_a[0] < stack_a[2]))
 	{
 		swap_stack_a(stack_a, numbers_in_stack_a);
 		rotate_stack_a(stack_a, numbers_in_stack_a);
 	}
 	if ((stack_a[0] < stack_a[1]) && (stack_a[1] > stack_a[2]) && (stack_a[0] > stack_a[2]))
-	{
 		reverse_rotate_stack_a(stack_a, numbers_in_stack_a);
-	}
 }
